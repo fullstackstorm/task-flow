1af5559bef9bf5a654cdd4cb1646dc79279ea0f1
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-09-30 12:10:50 -0700

    Create empty revision and create models.

---
+++ server/migrations/versions/8c28de5def4e_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 8c28de5def4e
+Revises: 
+Create Date: 2023-09-30 12:04:13.561982
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8c28de5def4e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
