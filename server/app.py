1dd5cd421e6c475bc5306f3fe744f13ee1afbacb
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-09-30 15:08:14 -0700

    Add some api endpoints.

--- server/app.py
+++ server/app.py
@@ -3,8 +3,9 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, make_response
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
@@ -16,6 +17,63 @@ from models import *
 def index():
     return '<h1>Project Server</h1>'
 
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+        password = json.get('password')
+
+        user = User(
+            username=json.get('username'),
+            email=json.get('email'),
+        )
+        user.password_hash = password
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return make_response(user.to_dict(), 201)
+        except IntegrityError as e:
+            db.session.rollback()
+            return make_response({'message': 'Invalid input'}, 422)
+        except Exception as e:
+            db.session.rollback()
+            return make_response({'message': 'An error occurred'}),
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return make_response(user.to_dict(), 200)
+        
+        return make_response({}, 401)
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 200)
+        
+        return make_response({}, 401)
+    
+class Logout(Resource):
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return make_response({'message': 'Logged out successfully'}, 204)
+        else:
+            return make_response({'error': 'Not logged in'}, 401)
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
