230fc4866d08882020963aac09bd422ddf114abf
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-10-03 18:05:44 -0700

    Implement the header.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div className="app">
+      <Header/>
+      <Switch>
+        <Route exact path="/">
+          
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
