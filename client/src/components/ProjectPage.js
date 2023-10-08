d1d26a6cc05d9a79c36eb750fa367c86f12d2eb0
Storm Anduaga-Arias <stormarias@gmail.com>
2023-10-08 14:42:24 -0700

    The buggy project page.

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import LeftNavigationBar from "./LeftNavigationBar";
 import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  display: flex;
+`;
 
 const BoxContainer = styled.div`
   background-color: #f5f5f5;
@@ -8,6 +13,7 @@ const BoxContainer = styled.div`
   border: 1px solid #ddd;
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 100%;
 `;
 
 const Heading = styled.h2`
@@ -38,40 +44,67 @@ const TableData = styled.td`
   padding: 10px;
 `;
 
-function ProjectPage() {
-  const { id } = useParams();
-  const [project, setProject] = useState({});
-
-  useEffect(() => {
-    // Fetch project data using the ID from the URL
-    fetch(`/projects/${id}`)
-      .then((response) => response.json())
-      .then((data) => setProject(data));
-  }, [id]);
-
+function TaskSection({ title, tasks }) {
   return (
-    <BoxContainer>
-      <Heading>{project.name}</Heading>
+    <div>
+      <h3>{title}</h3>
       <Table>
         <TableHead>
           <TableRow>
             <TableHeader>Task</TableHeader>
             <TableHeader>Due Date</TableHeader>
-            <TableHeader>Status</TableHeader>
           </TableRow>
         </TableHead>
         <tbody>
-          {project.tasks &&
-            project.tasks.map((task) => (
-              <TableRow key={task.id}>
-                <TableData>{task.title}</TableData>
-                <TableData>{new Date(task.due_date).toLocaleDateString()}</TableData>
-                <TableData>{task.status}</TableData>
-              </TableRow>
-            ))}
+          {tasks.map((task) => (
+            <TableRow key={task.id}>
+              <TableData>{task.title}</TableData>
+              <TableData>{new Date(task.due_date).toLocaleDateString()}</TableData>
+            </TableRow>
+          ))}
         </tbody>
       </Table>
-    </BoxContainer>
+    </div>
+  );
+}
+
+function ProjectPage() {
+  const { id } = useParams();
+  const [project, setProject] = useState({
+    pendingTasks: [],
+    inProgressTasks: [],
+    completedTasks: [],
+  });
+
+  useEffect(() => {
+    // Fetch project data using the ID from the URL
+    fetch(`/projects/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const tasks = data.tasks;
+        const pendingTasks = tasks.filter(task => task.status === 'pending');
+        const inProgressTasks = tasks.filter(task => task.status === 'in progress');
+        const completedTasks = tasks.filter(task => task.status === 'completed');
+  
+        setProject({
+          ...data, // Include other project data
+          pendingTasks,
+          inProgressTasks,
+          completedTasks,
+        });
+      });
+  }, [id]);
+
+  return (
+    <PageContainer>
+      <LeftNavigationBar />
+      <BoxContainer>
+        <Heading>{project.name}</Heading>
+        <TaskSection title="Pending Tasks" tasks={project.pendingTasks} />
+        <TaskSection title="In Progress Tasks" tasks={project.inProgressTasks} />
+        <TaskSection title="Completed Tasks" tasks={project.completedTasks} />
+      </BoxContainer>
+    </PageContainer>
   );
 }
 
